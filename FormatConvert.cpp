c3c8df44f95b6086de13974c1e7e8a4f75da8932
ameld <amel.delic@student.tugraz.at>
2016-04-13 02:27:16 +0200

    g

---
+++ FormatConvert.cpp
@@ -0,0 +1,6 @@
+#include "FormatConvert.h"
+
+FormatConvert::FormatConvert()
+{
+
+}
---
+++ FormatConvert.h
@@ -0,0 +1,11 @@
+#ifndef FORMATCONVERT_H
+#define FORMATCONVERT_H
+
+
+class FormatConvert
+{
+  public:
+    FormatConvert();
+};
+
+#endif // FORMATCONVERT_H
