4b18e83fae75c49b1864ea6b8d0f3a91105c375d
ameld <amel.delic@student.tugraz.at>
2016-04-13 02:35:09 +0200

    mm

--- converter.h
+++ converter.h
@@ -1,4 +1,24 @@
 #ifndef CONVERTER_H
 #define CONVERTER_H
 
+#include <QImage>
+#include <QVideoFrame>
+#include <opencv/highgui.h>
+#include <opencv/cv.hpp>
+
+#define CHANNEL_SIZE 8
+
+namespace convert {
+
+
+  //cv::Vec3b yuv;
+  //cv::Vec3b rgb;
+
+  bool frameToImage(QVideoFrame *video_frame, QImage &q_image);
+  QImage matToImage(cv::Mat *mat_image);
+  cv::Mat imageToMat(QImage *q_image);
+
+}
+
+
 #endif // CONVERTER_H
